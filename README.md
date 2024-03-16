ce6f4da795529e1f79c13482ef078c64034e89a8
Hector Armando Ramirez Estrada <137834404+ArmandoEstrada20@users.noreply.github.com>
2024-03-15 23:59:48 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+Bot de Telegram para la Alimentación de Mascotas
+
+## Resumen
+
+El objetivo de este proyecto es desarrollar un bot de Telegram interactivo y fácil de usar que proporcionará información precisa y útil sobre la alimentación de las mascotas.
+
+## Requisitos
+
+1. **Interactivo y fácil de usar**: El bot debe ser fácil de usar e interactivo, respondiendo a las preguntas de los usuarios de manera oportuna y precisa.
+
+2. **Información precisa y útil**: El bot proporcionará información precisa y útil sobre qué alimentos son seguros para las mascotas de los usuarios y cuáles podrían ser tóxicos o incluso mortales.
+
+3. **Cobertura amplia de mascotas**: El bot se diseñará para cubrir una amplia gama de mascotas y proporcionará información específica para cada tipo de mascota.
+
+4. **Respuestas detalladas y bien investigadas**: Los usuarios podrán hacer preguntas específicas y recibirán respuestas detalladas y bien investigadas.
+
+## Diseño de alto nivel
+
+El bot se implementará utilizando Python y la API de Telegram Bot. La base de datos de información sobre la alimentación de mascotas se almacenará en un arhcivo de tipo JSON.
+
+### Componentes principales
+
+1. **Interfaz de usuario**: Interfaz de chat de Telegram para interactuar con los usuarios.
+
+2. **Motor de chat**: Maneja la lógica de las conversaciones y genera respuestas a las preguntas de los usuarios.
+
+3. **Base de datos de alimentación de mascotas**: Almacena la información sobre qué alimentos son seguros y cuáles son tóxicos para diferentes tipos de mascotas.
+
+## Plan de implementación
+
+1. **Fase 1 - Diseño de la base de datos**: Diseñar y construir la base de datos de alimentación de mascotas.
+
+2. **Fase 2 - Implementación del motor de chat**: Desarrollar la lógica del bot para responder a las preguntas de los usuarios basándose en la información de la base de datos.
+
+3. **Fase 3 - Integración con Telegram**: Integrar el bot con Telegram y realizar pruebas exhaustivas.
+
+4. **Fase 4 - Lanzamiento y recopilación de feedback**: Lanzar el bot al público y recoger feedback para futuras mejoras.
---
+++ petBot.py
@@ -0,0 +1,81 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import json
+import re
+
+# Abre y carga el archivo JSON que contiene la información de los alimentos para mascotas
+with open('DatosMascotas/alimentos.json', encoding='utf-8') as file:
+    data = json.load(file)
+
+token = '7040554508:AAFUhP7cgQPH0j1DiA3aec9zGRsHIjmHfjk'
+usrName = 'PetFoodieBot'
+
+# Comandos de inicio
+async def start(update: Update, context: ContextTypes):
+    await update.message.reply_text('Hola, soy PetFoodieBot, te respondo preguntas respecto a la alimentación de tus mascotas. ¿En qué puedo ayudarte?')
+
+async def help(update: Update, context: ContextTypes):
+    await update.message.reply_text('Ayuda')
+
+def handle_response(text: str, context: ContextTypes, update: Update):
+    textoProcesado = text.lower()
+    print(textoProcesado)
+
+    # Busca si alguna de las palabras coincide con un tipo de mascota en el JSON
+    mascota_names = [k.lower() for k in data["Mascotas"].keys()]
+    for mascota in mascota_names:
+        if mascota in textoProcesado:
+            tipo_mascota = mascota
+            break
+    else:
+        return 'Por favor, especifica el tipo de mascota (por ejemplo, perro, gato, etc.)'
+
+    # Busca si alguna de las palabras coincide con un alimento en el JSON
+    alimentosBuenos = {alimento["nombre"].lower(): alimento for alimento in data["Mascotas"][tipo_mascota]["alimentosBuenos"]}
+    alimentosMalos = {alimento["nombre"].lower(): alimento for alimento in data["Mascotas"][tipo_mascota]["alimentosMalos"]}
+    for alimento in list(alimentosBuenos.keys()) + list(alimentosMalos.keys()):
+        if re.search(r'\b' + alimento + r'\b', textoProcesado):
+            alimento_nombre = alimento
+            descripcion = (alimentosBuenos[alimento] if alimento in alimentosBuenos else alimentosMalos[alimento])["descripcion"]
+            break
+    else:
+        return 'Por favor, especifica el alimento (por ejemplo, carne, chocolate, etc.)'
+
+    if alimento_nombre in alimentosBuenos:
+        return f'Sí, puedes darle {alimento_nombre} a tu {tipo_mascota}. {descripcion}'
+    elif alimento_nombre in alimentosMalos:
+        return f'No, no deberías darle {alimento_nombre} a tu {tipo_mascota}. {descripcion}'
+
+    return 'No entiendo tu pregunta, intentalo de nuevo...'
+
+async def handle_message(update: Update, context: ContextTypes):
+    message_type = update.message.chat.type
+    text = update.message.text
+
+    if message_type == 'group':
+        if text.startswith(usrName):
+            new_text = text.replace(usrName, '')
+            response = handle_response(new_text, context, update)
+        else:
+            return
+    else:
+        response = handle_response(text, context, update)
+
+    await update.message.reply_text(response)
+
+async def error(update: Update, context: ContextTypes):
+    print(context.error)
+    await update.message.reply_text('Ocurrió un error...')
+
+if __name__ == '__main__':
+    print('Bot iniciando...')
+    app = Application.builder().token(token).build()
+
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(CommandHandler('help', help))
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_error_handler(error)
+
+    print('El bot ha iniciado')
+    app.run_polling(poll_interval=1, timeout=10)
